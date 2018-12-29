c7a1ea6fb1bd17525cf974f687272dddc60d7d8e
Jan Oopkaup <jan.oopkaup@bringmeister.de>
2018-12-29 19:39:07 +0100

    Fix integration tests by adding additional auto configs
    (PS: Neither the IDE nor `gradle check` executes them. What the fuck?)

--- src/integrationTest/java/de/bringmeister/spring/aws/kinesis/JavaListenerTest.java
+++ src/integrationTest/java/de/bringmeister/spring/aws/kinesis/JavaListenerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.GenericContainer;
+import de.bringmeister.spring.aws.kinesis.creation.KinesisCreateStreamAutoConfiguration;
+import de.bringmeister.spring.aws.kinesis.validation.KinesisValidationAutoConfiguration;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -24,7 +26,9 @@ import java.util.function.Consumer;
         JacksonConfiguration.class,
         JacksonAutoConfiguration.class,
         KinesisLocalConfiguration.class,
-        AwsKinesisAutoConfiguration.class
+        AwsKinesisAutoConfiguration.class,
+        KinesisCreateStreamAutoConfiguration.class,
+        KinesisValidationAutoConfiguration.class
 })
 @RunWith(SpringRunner.class)
 public class JavaListenerTest {
