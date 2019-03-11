b3a909926fa49115bdd75344d5454a37f221d645
Thomas Uhrig <thomas.uhrig@bringmeister.de>
2019-03-11 18:14:04 +0100

    Build clients for worker manually

--- src/main/kotlin/de/bringmeister/spring/aws/kinesis/WorkerFactory.kt
+++ src/main/kotlin/de/bringmeister/spring/aws/kinesis/WorkerFactory.kt
@@ -1,5 +1,9 @@
 package de.bringmeister.spring.aws.kinesis
 
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessor
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.WorkerStateChangeListener
@@ -35,6 +39,30 @@ open class WorkerFactory(
             }
             .config(config)
             .recordProcessorFactory(processorFactory)
+            .kinesisClient(
+                AmazonKinesisClientBuilder
+                    .standard()
+                    .withCredentials(config.kinesisCredentialsProvider)
+                    .withClientConfiguration(config.kinesisClientConfiguration)
+                    .withEndpointConfiguration(EndpointConfiguration(config.kinesisEndpoint, settings.region))
+                    .build()
+            )
+            .dynamoDBClient(
+                AmazonDynamoDBClientBuilder
+                    .standard()
+                    .withCredentials(config.dynamoDBCredentialsProvider)
+                    .withClientConfiguration(config.dynamoDBClientConfiguration)
+                    .withEndpointConfiguration(EndpointConfiguration(config.dynamoDBEndpoint, settings.region))
+                    .build()
+            )
+            .cloudWatchClient(
+                AmazonCloudWatchClientBuilder
+                    .standard()
+                    .withCredentials(config.cloudWatchCredentialsProvider)
+                    .withClientConfiguration(config.cloudWatchClientConfiguration)
+                    .withRegion(settings.region)
+                    .build()
+            )
             .build()
     }
 
